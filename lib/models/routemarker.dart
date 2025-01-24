16d1a463194d294c8d244bd5845ceddd7a192d72
KaanCL <Kaancerkez34@outlook.com>
2025-01-25 02:07:20 +0300

    RouteMarker added

---
+++ lib/models/routemarker.dart
@@ -0,0 +1,62 @@
+import 'dart:ui';
+
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+  class RouteMarker extends Marker {
+    Set<Marker> markers;
+    Set<Polyline> polylines;
+    static int _idCounter = 0;
+    final int id;
+
+    RouteMarker({
+      double alpha = 1.0,
+      Offset anchor = const Offset(0.5, 1.0),
+      bool consumeTapEvents = false,
+      bool draggable = false,
+      bool flat = false,
+      BitmapDescriptor icon = BitmapDescriptor.defaultMarker,
+      InfoWindow infoWindow = InfoWindow.noText,
+      LatLng position = const LatLng(0.0, 0.0),
+      double rotation = 0.0,
+      bool visible = true,
+      double zIndex = 0.0,
+      String? clusterManagerId,
+      void Function()? onTap,
+      void Function(LatLng)? onDrag,
+      void Function(LatLng)? onDragStart,
+      void Function(LatLng)? onDragEnd,
+      required this.markers,
+      required this.polylines,
+    })
+        : id = _idCounter++,
+          super(
+          markerId: MarkerId("route_${_idCounter}"),
+          alpha: alpha,
+          anchor: anchor,
+          consumeTapEvents: consumeTapEvents,
+          draggable: draggable,
+          flat: flat,
+          icon: icon,
+          infoWindow: infoWindow,
+          position: position,
+          rotation: rotation,
+          visible: visible,
+          zIndex: zIndex,
+          onTap: onTap,
+          onDrag: onDrag,
+          onDragStart: onDragStart,
+          onDragEnd: onDragEnd,
+        );
+
+    Set<Marker> get getMarkers {
+    return markers;
+    }
+
+
+    Set<Polyline> get getPolylines {
+        return polylines;
+    }
+
+
+  }
--- lib/views/mapscreen.dart
+++ lib/views/mapscreen.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:pedal_istanbul/models/routemarker.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -17,9 +18,24 @@ class _MapScreenState extends State<MapScreen> {
  final Set<Polyline> _polylines = {};
  final Set<Marker> _markers = {};
 
+ final Set<Marker> _tempMarkers= {};
+ List<LatLng> _tempRoutePoints = [];
+
  int _polyLineIdCounter = 0;
  int _markerIdCounter = 0;
 
+ bool _isEditing = false;
+
+
+
+ @override
+  void initState() {
+   super.initState();
+   _isEditing = true;
+  }
+
+
+ RouteMarker? _selectedMarker = null;
 
  final LatLngBounds istanbulBounds = LatLngBounds(
        southwest: LatLng(40.8024, 28.5245),
@@ -32,11 +48,9 @@ class _MapScreenState extends State<MapScreen> {
       body: Stack(
         children: [
           GoogleMap(
-             initialCameraPosition: CameraPosition(
-              target: LatLng(41.0082, 28.9784),
-              zoom: 12),
-              polylines: _polylines,
-              markers: _markers,
+             initialCameraPosition: CameraPosition(target: LatLng(41.0082, 28.9784), zoom: 12),
+              polylines: _isEditing ? _polylines : (_selectedMarker?.getPolylines ?? {}),
+              markers:   _isEditing ? _tempMarkers : (_selectedMarker == null ? _markers : (_selectedMarker?.getMarkers ?? {})),
               onTap: _addRoutePoint,
               onMapCreated: (GoogleMapController controller) {_mapController = controller;},
               minMaxZoomPreference: MinMaxZoomPreference(10, 15),
@@ -56,10 +70,37 @@ class _MapScreenState extends State<MapScreen> {
                   onPressed:(){
                     setState(() {
                       _routePoints.clear();
-                      _polylines.clear();
-                      _markers.clear();
+                      if(_isEditing){
+                        _tempMarkers.clear();
+                        _polylines.clear();
+                      }else{
+                        _markers.clear();
+                        _isEditing = true;
+                      }
                   });} ,
                   child: Icon(Icons.cleaning_services_outlined),
+                ),
+                SizedBox(height: 5,),
+                if(_isEditing==false)
+                  FloatingActionButton(
+                    backgroundColor: Colors.deepOrange,
+                    onPressed: () {
+                      setState(() {
+                        _isEditing = true;
+                      });
+                    },
+                    child: Icon(Icons.add),
+                  ),
+                SizedBox(height: 5,),
+                if(_isEditing)
+                FloatingActionButton(
+                  onPressed:(){
+                    setState(() {
+                      _addRouteMarker();
+                      _isEditing=false;
+                      _selectedMarker=null;
+                    });} ,
+                  child: Icon(Icons.offline_pin),
                 ),
               ],
             ),
@@ -74,7 +115,9 @@ class _MapScreenState extends State<MapScreen> {
     setState(() {
       _routePoints.add(point);
 
-      _markers.add(Marker(
+      _tempRoutePoints = _routePoints;
+
+      _tempMarkers.add(Marker(
           markerId: MarkerId('marker_${_markerIdCounter++}'),
           position:point,
           infoWindow:InfoWindow(title: "Başlangıc"),
@@ -85,12 +128,12 @@ class _MapScreenState extends State<MapScreen> {
         _polylines.clear();
         _polylines.add(Polyline(
             polylineId: PolylineId('route_${_polyLineIdCounter++}'),
-            points: _routePoints,
+            points: _tempRoutePoints,
             color:Colors.blue,
             width: 5,
         ));
-        _markers.remove(_markers.elementAt(_markers.length-1));
-        _markers.add(Marker(
+        _tempMarkers.remove(_tempMarkers.elementAt(_tempMarkers.length-1));
+        _tempMarkers.add(Marker(
             markerId: MarkerId('marker'),
             position:_routePoints.last,
             infoWindow:InfoWindow(title: "varis"),
@@ -99,9 +142,57 @@ class _MapScreenState extends State<MapScreen> {
 
 
       }
-
     });
   }
+
+  void _addRouteMarker(){
+
+    _tempRoutePoints = _routePoints;
+    setState(() {
+
+    RouteMarker? routeMarker = null;
+    Marker? tempRouteMarker = null;
+    if (_routePoints.length < 2) return;
+
+    final midPoint = LatLng(
+      (_routePoints.first.latitude + _routePoints.last.latitude) / 2,
+      (_routePoints.first.longitude + _routePoints.last.longitude) / 2,
+    );
+
+
+    routeMarker = RouteMarker(
+       position: midPoint,
+       infoWindow:InfoWindow(title: "Rota Bilgisi"),
+       icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueOrange),
+       markers: Set.from(_tempMarkers),
+       polylines: Set.from(_polylines),
+       onTap: () {
+         _onMarkerTap(routeMarker!);
+       });
+
+    tempRouteMarker= Marker(
+        markerId: routeMarker.markerId,
+        position: midPoint,
+        infoWindow:InfoWindow(title: "Rota Bilgisi"),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueYellow),
+        onTap: () {
+          _onMarkerTap(routeMarker!);
+        });
+
+       _tempMarkers.clear();
+      _markers.add(routeMarker!);
+      _routePoints.clear();
+      _polylines.clear();
+    });
+  }
+
+ void _onMarkerTap(RouteMarker marker) {
+   setState(() {
+     print("Tapped marker position: ${marker.position}");
+     _selectedMarker = marker;
+     print("${marker.polylines.first.points}");
+   });
+ }
 
   void _clearRoute(){
     setState(() {
@@ -136,6 +227,7 @@ class _MapScreenState extends State<MapScreen> {
 
     });
   }
+
 
 
 
